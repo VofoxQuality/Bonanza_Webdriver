d5ea22c2a440737f441ab13a45c663bc0d178e68
VOFOX\Akhil-TS <akhilts@vofoxsolutions.con>
2018-10-11 19:47:56 +0530

    akhil-7:47

--- src/main/java/com/Home/Associate_Login.java
+++ src/main/java/com/Home/Associate_Login.java
@@ -14,9 +14,8 @@ public class Associate_Login extends UtilityClass
 	{
 		try
 		{
-			String title=driver.getTitle();
-			
-			Assert.assertEquals(title,"Log In | 5-15");
+			String title=driver.getTitle();			
+			Assert.assertEquals(title,"IGL");
 		} 
 		catch (Exception e)
 		{
@@ -43,10 +42,15 @@ public class Associate_Login extends UtilityClass
 			click("//*[@id='btnLogin']");
 			
 			//Assert message User name is required
-			Assert.assertTrue(getText("//*[@id='validation-message-0']").contains("is required"),"failed to assert text"+"  is required");
+			String U1=getText("//*[@id='rwUserName']/label");
+			String U2=getText("//*[@id='rwUserName']/label/following::p[1]");
+			Assert.assertEquals(U1+U2,"User Nameis required");
 			
 			//Assert message Password is required
-			Assert.assertTrue(getText("//*[@id='validation-message-3']").contains("is required"),"failed to assert text"+"  is required");
+			String P1=getText("//*[@id='rwPassword']/label");
+			String P2=getText("//*[@id='rwPassword']/label/following::p[1]");
+			Assert.assertEquals(P1+P2,"Passwordis required");
+			
 		} 
 		catch (Exception e) 
 		{
@@ -72,7 +76,9 @@ public class Associate_Login extends UtilityClass
 			click("//*[@id='btnLogin']");
 			
 			//Assert message User name is required
-			Assert.assertTrue(getText("//*[@id='validation-message-24']").contains("is required"),"failed to assert text"+"  is required");
+			String U1=getText("//*[@id='rwUserName']/label");
+			String U2=getText("//*[@id='rwUserName']/label/following::p[1]");
+			Assert.assertEquals(U1+U2,"User Nameis required");
 
 		} 
 		catch (Exception e) 
@@ -98,8 +104,10 @@ public class Associate_Login extends UtilityClass
 			//click Login button
 			click("//*[@id='btnLogin']");
 			
-			//Assert message User name is required
-			Assert.assertTrue(getText("//*[@id='validation-message-55']").contains("is required"),"failed to assert text"+"  is required");
+			//Assert message Password is required
+			String P1=getText("//*[@id='rwPassword']/label");
+			String P2=getText("//*[@id='rwPassword']/label/following::p[1]");
+			Assert.assertEquals(P1+P2,"Passwordis required");
 
 		} 
 		catch (Exception e) 
@@ -168,7 +176,7 @@ public class Associate_Login extends UtilityClass
 			
 			Thread.sleep(3000);
 			
-			//Assert message User name is required 
+			//Assert message Invalid username or password
 			Assert.assertTrue(getText("//*[@id='login-panel']/div/div[2]/div[1]").contains("Invalid username or password"),"failed to assert text"+"  Invalid username or password");
 
 		} 
@@ -178,6 +186,44 @@ public class Associate_Login extends UtilityClass
 			e.printStackTrace();
 		}
 		
+	}
+	
+	@Test(priority=6)	
+	public void TCBA01007()
+	{
+		
+		try {
+			//clear username field
+			driver.findElement(By.xpath("//*[@id='txtUserName']")).clear();
+			
+			//type Valid User Name
+			type("//*[@id='txtUserName']","Shinto-Test2");
+			
+			//clear Password field
+			driver.findElement(By.xpath("//*[@id='txtPassword']")).clear();	
+			
+			//Enter Invalid Password
+			type("//*[@id='txtPassword']","Password1232222");
+			
+			//click Login button correct user name and invalid password after three or more login attempt.]
+			click("//*[@id='btnLogin']");			
+			Thread.sleep(1000);
+			click("//*[@id='btnLogin']");
+			Thread.sleep(1000);
+			click("//*[@id='btnLogin']");
+			
+			//To verify the validation message "You have been locked out. Please contact support." 
+            String Blk_msg="You have been locked out. Please contact support.";
+			Assert.assertEquals(getText("//*[@id='login-panel']/div/div[2]/div[1]"),Blk_msg);
+		} 
+		
+		catch (Exception e)
+		{
+			Assert.fail(e.getMessage());
+			e.printStackTrace();
+		}
+
+	   
 	}
 	
 	@Test(priority=8)	
@@ -203,6 +249,8 @@ public class Associate_Login extends UtilityClass
 			
 			Thread.sleep(3000);
 			
+			// Assert the logout button is displayed in the dashboard
+			Assert.assertTrue(driver.findElement(By.xpath("//*[@id='navbar']/ul/li[4]/div/a/span[1]")).isDisplayed());						
 			
 		} 
 		catch (Exception e) 
