cd558fe7bb1395ca3a55473cd664216f5594c755
VOFOX\Akhil-TS <akhilts@vofoxsolutions.con>
2019-01-22 15:52:53 +0530

    commit

--- src/main/java/com/Library/UtilityClass.java
+++ src/main/java/com/Library/UtilityClass.java
@@ -72,9 +72,9 @@ public class UtilityClass	{
 		//Akhil 
 		System.setProperty("webdriver.gecko.driver", "E:\\Edinsight\\geckodriver.exe");
 		//Manoj
-		System.setProperty("webdriver.gecko.driver", "f:\\Jars\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "f:\\Jars\\geckodriver.exe");
 		//Server
-		System.setProperty("webdriver.gecko.driver", "e:\\Jars\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "e:\\Jars\\geckodriver.exe");
 		
 		
 		
--- src/main/java/com/associatePortal/Associate_Login.java
+++ src/main/java/com/associatePortal/Associate_Login.java
@@ -7,13 +7,13 @@ import org.testng.annotations.Test;
 import com.Library.UtilityClass;
 
 public class Associate_Login extends UtilityClass
-{
-	
+{	
 	@Test(priority=0)
 	public void TCBA01001()
 	{
 		try
 		{
+			Thread.sleep(2000);
 			String title=driver.getTitle();			
 			Assert.assertEquals(title,"Log In | 5-15");
 			
@@ -34,25 +34,19 @@ public class Associate_Login extends UtilityClass
 			//Assert message Password is required
 			String P1=getText("//*[@id='rwPassword']/label");
 			String P2=getText("//*[@id='rwPassword']/label/following::p[1]");
-			Assert.assertEquals(P1+P2,"Passwordis required");
-			
-			
+			Assert.assertEquals(P1+P2,"Passwordis required");				
 		} 
 		catch (Exception e)
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
 		}
-
 	}
-
 	@Test(priority=1)	
 	public void TCBA01002()
-	{
-		
+	{		
 		try
-		{
-			
+		{			
 			//Enter valid Password
 			type("//*[@id='txtPassword']","Password1!");			
 			
@@ -63,20 +57,16 @@ public class Associate_Login extends UtilityClass
 			String U1=getText("//*[@id='rwUserName']/label");
 			String U2=getText("//*[@id='rwUserName']/label/following::p[1]");
 			Assert.assertEquals(U1+U2,"User Nameis required");
-
 		} 
 		catch (Exception e) 
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
-		}
-		
-	}
-	
+		}		
+	}	
 	@Test(priority=2)	
 	public void TCBA01003()
-	{
-		
+	{		
 		try
 		{
 			//type valid User Name
@@ -98,17 +88,13 @@ public class Associate_Login extends UtilityClass
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
-		}
-		
-	}
-	
+		}		
+	}	
 	@Test(priority=3)	
 	public void TCBA01004()
-	{
-		
+	{		
 		try
-		{
-			
+		{			
 			//type invalid User Name
 			type("//*[@id='txtUserName']","dfgbfbgbnf");
 		
@@ -118,28 +104,22 @@ public class Associate_Login extends UtilityClass
 			//click Login button
 			click("//*[@id='btnLogin']");
 			
-			Thread.sleep(2000);
+			Thread.sleep(4000);
 			
 			//Assert message Username was not found
-			Assert.assertEquals(getText("//*[@id='login-panel']/div/div[2]/div[1]"), "Username was not found."," Username was not found-message not found");		
-			
-
+			Assert.assertEquals(getText("//*[@id='login-panel']/div/div[2]/div[1]"), "Username was not found.");			
 		} 
 		catch (Exception e) 
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
-		}
-		
-	}
-	
+		}		
+	}	
 	@Test(priority=4)	
 	public void TCBA01005()
-	{
-		
+	{		
 		try
-		{
-			
+		{			
 			//type Valid User Name
 			type("//*[@id='txtUserName']","Shinto-Test");
 			
@@ -150,10 +130,10 @@ public class Associate_Login extends UtilityClass
 			//click Login button
 			click("//*[@id='btnLogin']");
 			
-			Thread.sleep(3000);			
+			Thread.sleep(4000);			
 			
 			//Assert message Invalid username or password
-			Assert.assertEquals(getText("//div[@class='text-danger au-target']"), "Invalid username or password."," Invalid username or password.-message not found");
+			Assert.assertEquals(getText("//div[@class='text-danger au-target']"), "Invalid username or password.");
 			
 		} 
 		catch (Exception e) 
@@ -166,44 +146,36 @@ public class Associate_Login extends UtilityClass
 	
 	@Test(priority=5)	
 	public void TCBA01006()
-	{
-		
+	{		
 		try
-		{
-			
+		{			
 			//type InValid User Name
-			type("//*[@id='txtUserName']","fgfgfgfgf");
-			
+			type("//*[@id='txtUserName']","fgfgfgfgf");			
 			
 			//Enter Invalid Password
 			type("//*[@id='txtPassword']","dfdfdfdf");	
 			
 			//click Login button
-			click("//*[@id='btnLogin']");
-			
+			click("//*[@id='btnLogin']");			
 			Thread.sleep(3000);			
 			
 			//Assert message Username was not found
-			Assert.assertEquals(getText("//div[@class='text-danger au-target']"), "Username was not found."," Username was not found.-message not found");
-			
+			Assert.assertEquals(getText("//div[@class='text-danger au-target']"), "Username was not found."," Username was not found.-message not found");			
 		} 
 		catch (Exception e) 
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
-		}
-		
-	}
-	
+		}		
+	}	
 	@Test(priority=6)	
 	public void TCBA01007()
-	{
-		
-		try {
+	{		
+		try
+		{
 			
 			//type Valid User Name
-			type("//*[@id='txtUserName']","Shinto-Test2");
-			
+			type("//*[@id='txtUserName']","Shinto-Test2");			
 			
 			//Enter Invalid Password
 			type("//*[@id='txtPassword']","Password1232222");
@@ -219,23 +191,18 @@ public class Associate_Login extends UtilityClass
 			//To verify the validation message "You have been locked out. Please contact support." 
             String Blk_msg="You have been locked out. Please contact support.";
 			Assert.assertEquals(getText("//*[@id='login-panel']/div/div[2]/div[1]"),Blk_msg);
-		} 
-		
+		} 		
 		catch (Exception e)
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
-		}
-
-	   
+		}	   
 	}	
 	@Test(priority=7)	
 	public void TCBA01008()
-	{
-		
+	{		
 		try
-		{
-			
+		{			
 			//type Valid User Name
 			type("//*[@id='txtUserName']","Shinto-Test");
 			
@@ -255,8 +222,6 @@ public class Associate_Login extends UtilityClass
 		{
 			Assert.fail(e.getMessage());
 			e.printStackTrace();
-		}
-		
+		}		
 	}
-
 }
